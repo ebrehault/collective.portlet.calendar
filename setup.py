fd75bc2a0f477f6a5f9ad465159fc965977abbcd
hvelarde <hector.velarde@gmail.com>
2012-05-09 12:47:02 -0300

    updated package information and documentation

--- setup.py
+++ setup.py
@@ -1,39 +1,50 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-import os
 
-version = open(os.path.join("collective", "portlet", "calendar", "version.txt")).read().strip()
+import os
+from setuptools import setup, find_packages
+
+version = '0.6dev'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.portlet.calendar',
       version=version,
       description="Extended Calendar Portlet: A configurable implementation of a Calendar Portlet for Plone.",
-      long_description=open(os.path.join("collective", "portlet", "calendar", "README.txt")).read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
       classifiers=[
+        "Development Status :: 4 - Beta",
         "Framework :: Plone",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='plone plone4 portlet calendar web',
+      keywords='plone portlet calendar',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
-      url='https://bitbucket.org/simplesconsultoria/collective.portlet.calendar',
+      url='https://github.com/collective/collective.portlet.calendar',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.portlet'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
       entry_points="""
-      # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
       )
-
