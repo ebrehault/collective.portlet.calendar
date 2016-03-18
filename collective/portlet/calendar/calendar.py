38f5cc81252f76b51a3803308937730a1f6edce8
Eric BREHAULT <ebrehault@gmail.com>
2016-03-18 16:46:00 +0100

    replace CalendarTool getEventsForCalendar to use AdvancedQuery

--- collective/portlet/calendar/calendar.py
+++ collective/portlet/calendar/calendar.py
@@ -20,6 +20,7 @@ from plone.app.vocabularies.catalog import SearchableTextSourceBinder
 from plone.memoize import ram, instance
 from plone.memoize.compress import xhtml_compress
 from plone.portlets.interfaces import IPortletDataProvider
+from Products.AdvancedQuery import In
 from zope import schema
 from zope.component import getMultiAdapter
 from zope.formlib import form
@@ -165,6 +166,16 @@ class ICalendarExPortlet(IPortletDataProvider):
         value_type=schema.TextLine()
     )
 
+    exclude = schema.Bool(
+        title=_(u'Exclude keywords'),
+        description=_(
+            'help_exclude_keywords',
+            default=u'If selected, the keywords filter is reversed. '
+                    u'(only items which do NOT contain the listed keywords are '
+                    u'returned)'),
+        default=False,
+    )
+
 
 def untuple(options):
     """Seems that catalog only talk well with list, not tuples"""
@@ -182,12 +193,14 @@ class Assignment(base.Assignment):
     root = None
     review_state = ()
     kw = []
+    exclude = False
 
-    def __init__(self, name='', root=None, review_state=(), kw=[]):
+    def __init__(self, name='', root=None, review_state=(), kw=[], exclude=False):
         self.name = name
         self.root = root
         self.review_state = review_state
         self.kw = kw
+        self.exclude = exclude
 
     @property
     def title(self):
@@ -292,13 +305,131 @@ class Renderer(base.Renderer):
         weeks = self._get_calendar_structure()
         return weeks
 
+    def _get_events(self, month='1', year='2002', **kw):
+        """ recreates a sequence of weeks, by days each day is a mapping.
+            {'day': #, 'url': None}
+        """
+        year = int(year)
+        month = int(month)
+        # daysByWeek is a list of days inside a list of weeks, like so:
+        # [[0, 1, 2, 3, 4, 5, 6],
+        #  [7, 8, 9, 10, 11, 12, 13],
+        #  [14, 15, 16, 17, 18, 19, 20],
+        #  [21, 22, 23, 24, 25, 26, 27],
+        #  [28, 29, 30, 31, 0, 0, 0]]
+        daysByWeek = self.calendar._getCalendar().monthcalendar(year, month)
+        weeks = []
+
+        last_day = self.calendar._getCalendar().monthrange(year, month)[1]
+        first_date = self.calendar.getBeginAndEndTimes(1, month, year)[0]
+        last_date = self.calendar.getBeginAndEndTimes(last_day, month, year)[1]
+
+        query_args = {
+            'portal_type': self.calendar.getCalendarTypes(),
+            'review_state': self.calendar.getCalendarStates(),
+            'start': {'query': last_date, 'range': 'max'},
+            'end': {'query': first_date, 'range': 'min'},
+            'sort_on': 'start'}
+        query_args.update(kw)
+
+        ctool = getToolByName(self, 'portal_catalog')
+        if 'Subject' in query_args and len(query_args['Subject']) > 0 and self.data.exclude:
+            params = {}
+            for key in query_args:
+                if key == "Subject":
+                    continue
+                params[key] = query_args[key]
+            base_query = ctool.makeAdvancedQuery(params)
+            query = ctool.evalAdvancedQuery(base_query & ~In('Subject', query_args['Subject']))
+        else:
+            query = ctool(**query_args)
+
+        # compile a list of the days that have events
+        eventDays = {}
+        for daynumber in range(1, 32):  # 1 to 31
+            eventDays[daynumber] = {'eventslist': [],
+                                    'event': 0,
+                                    'day': daynumber}
+        includedevents = []
+        for result in query:
+            if result.getRID() in includedevents:
+                break
+            else:
+                includedevents.append(result.getRID())
+            event = {}
+            # we need to deal with events that end next month
+            if result.end.month() != month:
+                # doesn't work for events that last ~12 months
+                # fix it if it's a problem, otherwise ignore
+                eventEndDay = last_day
+                event['end'] = None
+            else:
+                eventEndDay = result.end.day()
+                event['end'] = result.end.Time()
+            # and events that started last month
+            if result.start.month() != month:  # same as above (12 month thing)
+                eventStartDay = 1
+                event['start'] = None
+            else:
+                eventStartDay = result.start.day()
+                event['start'] = result.start.Time()
+
+            event['title'] = result.Title or result.getId
+
+            if eventStartDay != eventEndDay:
+                allEventDays = range(eventStartDay, eventEndDay + 1)
+                eventDays[eventStartDay]['eventslist'].append(
+                        {'end': None,
+                         'start': result.start.Time(),
+                         'title': event['title']})
+                eventDays[eventStartDay]['event'] = 1
+
+                for eventday in allEventDays[1:-1]:
+                    eventDays[eventday]['eventslist'].append(
+                        {'end': None,
+                         'start': None,
+                         'title': event['title']})
+                    eventDays[eventday]['event'] = 1
+
+                if result.end == result.end.earliestTime():
+                    last_day_data = eventDays[allEventDays[-2]]
+                    last_days_event = last_day_data['eventslist'][-1]
+                    last_days_event['end'] = \
+                        (result.end - 1).latestTime().Time()
+                else:
+                    eventDays[eventEndDay]['eventslist'].append(
+                        {'end': result.end.Time(),
+                         'start': None, 'title': event['title']})
+                    eventDays[eventEndDay]['event'] = 1
+            else:
+                eventDays[eventStartDay]['eventslist'].append(event)
+                eventDays[eventStartDay]['event'] = 1
+            # This list is not uniqued and isn't sorted
+            # uniquing and sorting only wastes time
+            # and in this example we don't need to because
+            # later we are going to do an 'if 2 in eventDays'
+            # so the order is not important.
+            # example:  [23, 28, 29, 30, 31, 23]
+
+        for week in daysByWeek:
+            days = []
+            for day in week:
+                if day in eventDays:
+                    days.append(eventDays[day])
+                else:
+                    days.append({'day': day, 'event': 0, 'eventslist': []})
+
+            weeks.append(days)
+
+        return weeks
+
     def _get_calendar_structure(self):
         context = aq_inner(self.context)
         year = self.year
         month = self.month
         if self.options.get('Subject', None):
             self.options['Subject'] = [el.encode('utf-8') for el in self.options['Subject']]
-        weeks = self.calendar.getEventsForCalendar(month, year, **self.options)
+        weeks = self._get_events(month, year, **self.options)
         for week in weeks:
             for day in week:
                 daynumber = day['day']
